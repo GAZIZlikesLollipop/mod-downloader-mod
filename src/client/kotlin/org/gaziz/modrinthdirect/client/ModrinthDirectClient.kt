016595d4ef7dbb8af385859ed0ac02958d721dbb
GAZIZlikesLollipop <gazizvalitov230378@gmail.com>
2026-08-02 11:43:06 +0500

    initial commit

---
+++ src/client/kotlin/org/gaziz/modrinthdirect/client/ModrinthDirectClient.kt
@@ -0,0 +1,7 @@
+package org.gaziz.modrinthdirect.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object ModrinthDirectClient : ClientModInitializer {
+	override fun onInitializeClient() {}
+}
---
+++ src/main/kotlin/org/gaziz/modrinthdirect/ModrinthDirect.kt
@@ -0,0 +1,10 @@
+package org.gaziz.modrinthdirect
+
+import net.fabricmc.api.ModInitializer
+import net.minecraft.resources.Identifier
+import org.slf4j.LoggerFactory
+
+object ModrinthDirect : ModInitializer {
+	const val MOD_ID: String = "modrinth-direct"
+	override fun onInitialize() {}
+}
